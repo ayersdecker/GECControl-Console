27898f431b04afb07a62ef2d21bbe7178c309e8f
ayersdecker <ayersdecker@gmail.com>
2023-06-22 20:41:49 -0400

    GE Enpoints and Login GE Request Finished

---
+++ GE C/GE C/GE_Endpoints.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GE_C
+{
+    public class GE_Endpoints
+    {
+        //
+        // Login Endpoint that returns Access-Token
+        //
+        public string LoginURL()
+        {
+            return "https://api2.xlink.cn/v2/user_auth";
+        }
+        //
+    // Grabs Device List for Specified User
+    //
+        public string GetDevicesURL(string user)
+        {
+            return "https://api2.xlink.cn/v2/user/" + $"{user}" + "/subscribe/devices";
+        }
+        //
+    // Grabs Device Properties for Specified Product
+    //
+        public string GetPropertiesURL(string productId, string deviceId)
+        {
+            return "https://api2.xlink.cn/v2/product/" + $"{productId}" + "/device/" + $"{deviceId}" + "/property";
+        }
+        //
+        // Grabs User Information for Specified User
+        //
+        public string GetUserURL(string user)
+        {
+            return "https://api2.xlink.cn/v2/user/" + $"{user}";
+        }
+    }
+}
---
+++ GE C/GE C/GE_Requests.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GE_C
+{
+    public class GE_Requests
+    {
+        public GE_Endpoints endpoints = new GE_Endpoints();
+        public async Task<dynamic> Login(string username, string password)
+        {
+
+            // JSON Format
+            var json = "{ \"corp_id\": \"1007d2ad150c4000\", \"email\": " + $"{username}" + ", \"password\": " + $"{password}" + "}";
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(endpoints.LoginURL()),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+            // Builds Request
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion");
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            // Get JSON Data
+            string content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<dynamic>(content);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
